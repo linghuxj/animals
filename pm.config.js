897defd7215265cd2f7760a4295c81548ba6c875
linghuxj <linghuxj@gmail.com>
2024-07-23 00:52:46 +0800

    pm2 config change

--- pm.config.js
+++ pm.config.js
@@ -1,4 +1,5 @@
 const { exec } = require("child_process");
+const { ar } = require("date-fns/locale");
 const { watch } = require("fs");
 const { env } = require("process");
 
@@ -7,12 +8,14 @@ module.exports = {
     {
       name: "animals",
       script: "./.next/server/pages/_app.js",
+      args: "start --port 3000",
       env: {
         NODE_ENV: "development",
       },
       watch: false,
       instances: "max",
       exec_mode: "cluster",
+      log_file: "logs/animals.log",
     },
   ],
 };
