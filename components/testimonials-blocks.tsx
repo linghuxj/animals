f80c3d850da513963145301535e2d8f5a01ca714
linghuxj <linghuxj@gmail.com>
2024-07-24 11:25:13 +0800

    mod

--- components/testimonials-blocks.tsx
+++ components/testimonials-blocks.tsx
@@ -95,9 +95,9 @@ export default function TestimonialsBlocks() {
                   <path className="fill-current text-purple-500" d="M2.785 5.334C2.538 5.5-.2 2.944.011 2.646.826 1.483 2.183.836 3.62.5 5.064.158 6.582.117 7.92-.02c.017-.002.098.153.088.166-1.763 2.018-3.223 3.836-5.221 5.188zm3.676 6.519c-.862.184-1.937-3.403-1.07-3.711 3.422-1.22 7.078-1.671 10.728-1.766 3.655-.096 7.304.162 10.866.32.044.002.06.177.018.187-6.938 1.634-13.691 3.504-20.542 4.97z" />
                 </svg>
               </div>
-              <blockquote className="text-xl text-gray-600 dark:text-gray-400">“ 这项技术对我们的家庭来说是一个巨大的福音。希望它能尽快问世，让我们能够更好地与宠物交流，共享美好时光。 “</blockquote>
+              <blockquote className="text-xl text-gray-600 dark:text-gray-400">“ 这个项目让我看见了让孩子更少接触电子产品，比如少用手机、电脑的可能性，能更多的引导孩子在户外学习玩耍的时间。 “</blockquote>
               <div className="font-red-hat-display font-bold mt-2">
-                <cite className="not-italic">—Miss. 谢</cite>, <a className="text-teal-500 transition duration-150 ease-in-out" href="#0">Appicu</a>
+                <cite className="not-italic">—Miss. 谢</cite>, <a className="text-teal-500 transition duration-150 ease-in-out" href="#0">Fans</a>
               </div>
             </div>
 
